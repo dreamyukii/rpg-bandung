a20242838d550cc42a7ee31c1f57c19e7253f595
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2024-07-26 00:49:05 +0700

    feat: add proper main menu

---
+++ scene/about.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _on_play_pressed():
+	SceneTransition.Transit("res://scene/main_menu.tscn")
+	
--- scene/scripts/main_menu.gd
+++ scene/scripts/main_menu.gd
@@ -1,15 +1,17 @@
 extends Control
 
-#@onready var animation = $AnimationPlayer
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#SceneTransition.SceneTransit("res://scene/scene1.tscn")
 	pass
 
 func _on_play_pressed():
-	#SceneTransition._in("res://scene/scene1.tscn")
 	SceneTransition.Transit("res://scene/Scene1.tscn")
+	
+func _on_about_pressed():
+	SceneTransition.Transit("res://scene/about.tscn")
+	
+func _on_exit_pressed():
+	get_tree().quit()
 	
 	
 	
